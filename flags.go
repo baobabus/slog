501e744ae8c0fdd01d0bd5d72e6bf1a5c3a7381c
Aleksey Blinov <a6a6un@yahoo.com>
2016-02-22 12:33:55 -0600

    Improved flag description messages.

--- flags.go
+++ flags.go
@@ -8,8 +8,8 @@ import (
 )
 
 func init() {
-	flag.StringVar(&rtLevel, "loglevel", "info", "set logging level; supported values are \"error\", \"warn\", \"notice\" and \"info\"")
-	flag.UintVar(&rtTrace, "trace", 0, "enable trace logging with specified verbosity")
-	flag.StringVar(&rtFormat, "logfmt", "simple", "set logging format; supported values are \"simple\", \"json\" and \"json-pretty\"")
-	flag.StringVar(&rtLog, "log", "stderr", "send log output to specified file; recognized special values are \"stdout\", \"stderr\" and \"syslog\"")
+	flag.StringVar(&rtLevel, "loglevel", "info", "set logging `level`; supported values are \"error\", \"warn\", \"notice\" and \"info\"")
+	flag.UintVar(&rtTrace, "trace", 0, "enable trace logging with specified `verbosity`")
+	flag.StringVar(&rtFormat, "logfmt", "simple", "set logging `format`; supported values are \"simple\", \"json\" and \"json-pretty\"")
+	flag.StringVar(&rtLog, "log", "stderr", "set log output to `destination`, where destination is a filename or one of \"stdout\", \"stderr\" or \"syslog\"")
 }
